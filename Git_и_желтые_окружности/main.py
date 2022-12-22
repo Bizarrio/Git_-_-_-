becc60f97d25b06cb978d96f60b1363e08a01196
Alex <Sashaosin27@yandex.ru>
2022-12-22 15:48:50 +0300

    program with yellow circles

---
+++ Git_и_желтые_окружности/main.py
@@ -0,0 +1,64 @@
+import sys
+import random
+
+from PyQt5 import uic
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(800, 600)
+        Form.setMinimumSize(QSize(800, 600))
+        Form.setMaximumSize(QSize(800, 600))
+        self.pushButton = QPushButton(Form)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(600, 490, 171, 81))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Form", None))
+        self.pushButton.setText(QCoreApplication.translate("Form", u"Show Circle", None))
+    # retranslateUi
+
+
+class Example(QMainWindow, Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.clicked = False
+        self.setWindowTitle('Git и желтые окружности')
+        self.pushButton.clicked.connect(self.make_circle)
+        self.x, self.y = None, None
+        self.circle_size = 0
+        self.color = QColor(255, 255, 0)
+
+    def make_circle(self):
+        self.circle_size = random.randint(50, 150)
+        self.color = self.color
+        self.clicked = True
+        self.update()
+
+    def paintEvent(self, event):
+        if self.clicked:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setPen(self.color)
+            qp.setBrush(self.color)
+            qp.drawEllipse(random.randint(100, 500), random.randint(100, 500), self.circle_size, self.circle_size)
+            qp.end()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec_())
